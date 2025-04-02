267d88386e837f00cd5db28a02e88370abc143b7
Kyeongtaek Lee <skylove0911@naver.com>
2025-04-03 02:03:49 +0900

    [feat] UI에 컴포넌트 추가

--- Week0v2/Engine/Source/Runtime/Engine/Classes/Engine/ResourceMgr.cpp
+++ Week0v2/Engine/Source/Runtime/Engine/Classes/Engine/ResourceMgr.cpp
@@ -33,6 +33,9 @@ void FResourceMgr::Initialize(FRenderer* renderer, FGraphicsDevice* device)
 	LoadTextureFromDDS(device->Device, device->DeviceContext, L"Assets/Texture/UUID_Font.dds");
 	LoadTextureFromFile(device->Device, device->DeviceContext, L"Assets/Texture/Wooden Crate_Crate_BaseColor.png");
 	LoadTextureFromFile(device->Device, device->DeviceContext, L"Assets/Texture/spotLight.png");
+    LoadTextureFromFile(device->Device, device->DeviceContext, L"Assets/Texture/Pawn_64x.png");
+    LoadTextureFromFile(device->Device, device->DeviceContext, L"Assets/Texture/PointLight_64x.png");
+    LoadTextureFromFile(device->Device, device->DeviceContext, L"Assets/Texture/SpotLight_64x.png");
 }
 
 void FResourceMgr::Release(FRenderer* renderer) {
