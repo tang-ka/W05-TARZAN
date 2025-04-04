148b4753c335ec2ad945e5bacff9cc92c28a1582
Friedonion <rkdwnd333@naver.com>
2025-04-05 01:18:17 +0900

    파이어볼 색상 변경 UI 추가

--- TARZAN/Engine/Source/Runtime/Launch/Define.h
+++ TARZAN/Engine/Source/Runtime/Launch/Define.h
@@ -262,7 +262,7 @@ struct FCone
 
     FVector ConeBaseCenter; // 원뿔 밑면 중심
     float ConeHeight; // 원뿔 높이 (Apex와 BaseCenter 간 차이)
-    FVector4 Color;
+    FLinearColor Color;
 
     int ConeSegmentCount; // 원뿔 밑면 분할 수
     float pad[3];
